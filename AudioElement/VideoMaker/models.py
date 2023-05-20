c2e0a7281437eb71d8664a998977926c6b43e739
Muh Roshan <leoparmigiana@gmail.com>
2023-05-20 13:01:49 +0530

    VidioMaker models created | not migrated

--- AudioElement/VideoMaker/models.py
+++ AudioElement/VideoMaker/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class VideoMaker(models.Model):
+    # "type": <vo|bg_music|video_music>",
+    CHOICE_SELECT = (
+        ('vo'),
+        ('bg_music'),
+        ('video_music'),
+    )
+    type = models.CharField(max_length=15,choices=CHOICE_SELECT)
+    high_volume = models.PositiveIntegerField()
+    low_volume = models.PositiveIntegerField()
+    video_component_id = models.CharField(max_length=300,null=True,blank=True)
+    url = models.URLField(blank=True,null=True)
+    start_time = models.PositiveIntegerField(blank=True,null=True)
+    end_time = models.PositiveIntegerField(blank=True,null=True)
+
+    def __str__(self):
+        return self.id
