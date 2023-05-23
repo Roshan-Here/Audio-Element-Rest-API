3c5cee1c98358c441d74b2a3f538131695be4289
Muh Roshan <leoparmigiana@gmail.com>
2023-05-23 11:17:07 +0530

    Re-migrated added new datas | checked using all crud methods

--- AudioElement/checking/base.py
+++ AudioElement/checking/base.py
@@ -2,15 +2,31 @@ import requests
 
 endpoint = 'http://localhost:8000/Api/VideoMaker/'
 
-data = {
-    "typee":'vo',
-    "high_volume":45,
-    "low_volume":12,
-    'duration': {
-        'start_time':  10,
-        'end_time': 50,
-    }
+data = {    
+			"typee": "video_music",
+			"video_component_id": "xyz",
+			"high_volume": 50,
+			"low_volume": 50,
+			"duration": {
+                "start_time": 0,
+				"end_time": 0,
+            },
+			"url": ""
 }
+
+
+		# {
+		# 	"id": "123",
+		# 	"type": "vo",
+		# 	"video_component_id": null,
+		# 	"high_volume": 100,
+		# 	"low_volume": 75,
+		# 	"duration": {
+		# 		"start_time": 0,
+		# 		"end_time": 20
+		# 	},
+		# 	"url": ""
+		# }
 
 result = requests.post(endpoint,json=data)
 
